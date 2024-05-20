6c7d345e0415e8263ffad2c6e7e3db79566c3101
Ayda Gonzalez <aydagonzalez15@gmail.com>
2024-05-20 19:00:17 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/NavBar';
 import About from './components/About';
-import Technology from './components/Technology';
+// import Technology from './components/Technology';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Experience from './components/Experience';
+// import Experience from './components/Experience';
 import './App.css';
 
 export default function App() {
@@ -18,7 +18,7 @@ export default function App() {
             <Route path="/" element={<About />} />
             {/* <Route path="/technology" element={<Technology />} /> */}
             <Route path="/projects" element={<Projects />} />
-            <Route path="/experience" element={<Experience />} />
+            {/* <Route path="/experience" element={<Experience />} /> */}
             <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
