7e1b32ad882d525e17ecf8a2eb0c50bbe625f953
Ayda Gonzalez <aydagonzalez15@gmail.com>
2024-10-08 17:45:11 -0400

    added breatheWell app

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,6 +5,47 @@ export default function Projects() {
         <>
             <h1 className='name-header'> Software Engineering Projects</h1>
             <section className="projects-section">
+
+            <div className="project-card">
+                    <h2>BreatheWell</h2>
+                    <img src="./portfolio/breathewell.png" alt="breatheWell application snapshot" />
+                    <p>Air quality tracker app that provides live AQI updates, personalized health recommendations, and a map that automatically updates based on location.
+                         Utilized MongoDB, Express, React, and Node (MERN stack), Chart.js.</p>
+
+
+                    <div className="tech-container">
+                        <div className="tech-item2">
+                            <img src="./portfolio/logo512.png" alt="React logo" className="tech-logo2" /> React
+                        </div>
+                        <div className="tech-item2">
+                            <img src="./portfolio/js.png" alt="JavaScript logo" className="tech-logo2" /> JavaScript
+                        </div>
+                        <div className="tech-item2">
+                            <img src="./portfolio/express.png" alt="Express logo" className="tech-logo2" /> Express
+                        </div>
+                        <div className="tech-item2">
+                            <img src="./portfolio/mongodb.png" alt="MongoDB logo" className="tech-logo2" /> MongoDB
+                        </div>
+                        <div className="tech-item2">
+                            <img src="./portfolio/mongoose.png" alt="Mongoose logo" className="tech-logo2" /> Mongoose
+                        </div>
+                        <div className="tech-item2">
+                            <img src="./portfolio/css.png" alt="CSS logo" className="tech-logo2" /> CSS
+                        </div>
+                        <div className="tech-item2">
+                            <img src="./portfolio/html.png" alt="HTML logo" className="tech-logo2" /> HTML
+                        </div>
+                        <div className="tech-item2">
+                            <img src="./portfolio/node3.png" alt="Node.js logo" className="tech-logo2" /> Node
+                        </div>
+                    </div>
+
+                    <div className="button-container">
+                        <a href="https://ga-enviro-hackathon-wind2024.onrender.com/" className="button">Live App</a>
+                        <a href="https://github.com/aydagonzalez/GA-Enviro-Hackathon-Wind2024" className="button github-button">GitHub</a>
+                    </div>
+                </div>
+
                 <div className="project-card">
                     <h2>Date Night App</h2>
                     <img src="./portfolio/date-night-app.png" alt="date-night app snapshot" />
